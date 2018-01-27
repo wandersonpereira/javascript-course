5cf02a7d2a6aeb7704bdb23a4d66be2ff058eb09
Wanderson Pereira <wandersonpereira@ivendas.com.br>
2018-01-27 18:17:38 -0200

    Funções

---
+++ functions/README.md
@@ -0,0 +1,1 @@
+# FUNÇÕES JAVASCRIPT
---
+++ functions/functions.js
@@ -0,0 +1,22 @@
+
+function nomeDaMinhaFuncao (PARAMETROS) {
+    //MINHAS AÇOES
+}
+    
+//Exemplos:
+//Cria a função que calcula a minha idade, recebendo o ano de meu nascimento
+function minhaIdade(anoNascimento) {
+// Ano atual (2018) - Ano de meu nascimento
+return 2018 - anoNascimento;
+}
+//Imprime o resultado da função
+alert(minhaIdade(1990));
+
+//Função que imprime meu nome
+//Nota-se que no caso desta função não recebe paramêtros.
+function buscarMeuNome() {
+    // Retorna meu nome completo
+    return 'Wanderson Pereira';
+}
+// Imprime o resultado da função
+alert(buscarMeuNome());
